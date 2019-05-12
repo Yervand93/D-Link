86d41749053dae7486fa7900e1fb1cb21efd4b42
yervand <ervandmar@gmail.com>
2019-05-12 13:08:49 +0400

    add functional for copy txt to xlsx file

--- Python/convert.py
+++ Python/convert.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 
 from os import path
 import sys
@@ -7,36 +8,29 @@ import argparse
 def createParser():
 	parser=argparse.ArgumentParser()
 	parser.add_argument('-f','--file',dest='file_value',type=argparse.FileType(),help="Read file is txt")
-	parser.add_argument('-x','--xlsx',dest='xlsx_value',type=argparse.FileType(),help="Write txt file to xlsx")	
+	parser.add_argument('-x','--xlsx',dest='xlsx_value',type=argparse.FileType('w'),help="Write txt file to xlsx")	
 	return parser
 
-def txt_file(x):
-	r=open(x)
-	s=r.read()
-	print(s)	
-	
-
-
-
-
-
-
-
+def txt_file(a,b):
+	r=open(a,"r")
+	w=open(b,"w")
+	if b[-4:]=="xlsx":	
+		s=w.write(r.read())
+		print("Your -xlsx- file is done")
+	else:
+		print("The file what you are  wrie is not -xlsx- file")
 
 
 def main():
 	parser=createParser()
 	namespace=parser.parse_args(sys.argv[1:])
 	f=namespace.file_value.name
-	#x=namespace.xlsx_value.name
+	x=namespace.xlsx_value.name
 	if f[-3:]=="txt" and path.exists(f):
-		txt_file(f)
+		txt_file(f,x)
 	else:
 		print("Edpisi fayl chka")
-	print("-------- = {}".format(namespace.file_value.name))
-	#text=namespace.file.read()
-	#print(text)
-#	with open('text')
+#	print("-------- = {}".format(namespace.file_value.name))
 
 
 
