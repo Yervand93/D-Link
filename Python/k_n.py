c2aaae4502070fe9c1baaff7077d76bd3ca68920
yervand <ervandmar@gmail.com>
2019-05-18 02:47:53 +0400

    2 funkciayi tex me player funkcya

--- Python/k_n.py
+++ Python/k_n.py
@@ -3,8 +3,9 @@
 from random import randint
 from time import sleep
 
+empty_global=""
 board_num=[1,2,3,4,5,6,7,8,9]
-combinacya=[[0,1,2],[3,4,5],[6,7,8],[0,4,8],[0,3,6],[1,4,7],]
+combinacya=[[0,1,2],[3,4,5],[6,7,8],[0,4,8],[0,3,6],[1,4,7]]
 def board():
 		print("|",(board_num[0]),"|",(board_num[1]),"|",(board_num[2]),"|")
 		print("-------------")
@@ -13,41 +14,48 @@ def board():
 		print("|",(board_num[6]),"|",(board_num[7]),"|",(board_num[8]),"|")
 		print("-------------")
 
-def player(number):
-	if number in board_num:
-		b_n=board_num.index(number)
-		board_num[b_n]="X"
-		board()
-		return
-	else:
-		print("You write number out in range 1-9 or your number is used.")
+#def player(number):
+#	if number in board_num:
+#		b_n=board_num.index(number)
+#		board_num[b_n]="X"
+#		board()
+#		return
+#	else:
+#		print("You write number out in range 1-9 or your number is used.")
 	
-def computer():
+def playing(arg=empty_global):
 	number=randint(1,9)
-	if number in board_num:
+	if (number or empty_global) in board_num:
 		b_n=board_num.index(number)
-		board_num[b_n]="O"
-		board()
-		return
+		if arg is empty_global:
+			b_np=board_num.index(empty_global)
+			board_num[b_np]="X"
+			board()
+			return
+		else:
+			board_num[b_n]="O"
+			board()
+			return
 	else:
 		print("You write number out in range 1-9 or your number is used.")
 	
 
 
 def gaming(name):
-	while not sxal:
-		p=input("{} write number 1-9: ".format(name))
+	while True:
+		global empty_global
+		players_in=input("{} write number 1-9: ".format(name))
 		try:
-			p=int(p)
+			players_in=int(players_in)
 		except ValueError:
 			pass
-		player(p)
+		empty_global=players_in	
+		playing(players_in)
 		print("Now turn Computer")
 		sleep(0.3)
-		computer()
+		playing()
 
 
-def win_game():
 	
 
 
