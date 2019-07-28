26a9b65fb043890cf15c81a8ff5282d3c1facc63
yervand <ervandmar@gmail.com>
2019-07-28 17:24:46 +0400

    delete and modified

--- C++/calc_in_terminal/main.cpp
+++ C++/calc_in_terminal/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "my_string.hpp"
+#include "lib/my_string.hpp"
 
 
 int main(int argc,char* argv[])
@@ -9,8 +9,8 @@ int main(int argc,char* argv[])
 	char* mas2 = new char[20];
 	int count = 0; 
 	char oper;	// this variable for operators(+; -; *; /) 
-	double pat = 0; // variable for numbers at left side operator
-	double pat_do = 0; // variable for numbers at right side operator
+	int pat = 0; // variable for numbers at left side operator
+	int pat_do = 0; // variable for numbers at right side operator
 	
 	all_argv_to_array(arr,argv,count); // all arguments of terminal append in my array
 
@@ -19,8 +19,14 @@ int main(int argc,char* argv[])
 	pat = my_string_to_int(mas1); // from funcion, replace string numbers to int 
 	pat_do = my_string_to_int(mas2); // from function, replace string numbers to int
 	
-	std::cout << pat << oper << pat_do << " = " << my_switch(oper,pat,pat_do) << std::endl; // printing result 
-
+	if(oper == '+' || oper == '-' || oper == '*' || oper == '/')
+	{	
+		std::cout << pat << oper << pat_do << " = " << my_switch(oper,pat,pat_do) << std::endl; // printing result 
+	}
+	else
+	{
+		std::cout << "You must be write operators!!!" << std::endl;
+	}
 	delete[] arr;
 	delete[] mas1;
 	delete[] mas2;	
